0a409583f55b31eedaf5911b36754e36acc07b8e
arbrown110 <arbrown110@gmail.com>
2020-07-10 12:26:32 -0400

    traxes controller updated

--- app/controllers/traxes_controller.rb
+++ app/controllers/traxes_controller.rb
@@ -1,37 +1,84 @@
 class TraxesController < ApplicationController
 
-  # GET: /traxes
-  get "/traxes" do
-    erb :"/traxes/index"
-  end
+      #create
+      get '/traxes/new' do
+        if signed_in?
+            @traxes = Traxes.all
+            erb :'traxes/index'
+        else
+            erb :'users/sign_in'
+        end
+    end
+    
+    post '/traxes' do
+        if params.values.any? {|value| value == ""}
+            erb :'traxes/new'
+        else
+            user = User.find(session[:user_id])
+            @traxes = Traxes.create(
+                name: params[:name], date: params[:date], 
+                score: params[:score], location: params[:location], 
+                number: params[:number], interest: params[:interest]
+            )
+            redirect to "/traxes/#{@traxes.id}"
+        end
+    end
 
-  # GET: /traxes/new
-  get "/traxes/new" do
-    erb :"/traxes/new"
-  end
+    #review
+    get '/traxes/:id' do
+      if signed_in?
+        @traxes = Traxes.find(params[:users_id]) 
+        erb :'/traxes/show'
+      else
+         erb :'users/sign_in' 
+      end
 
-  # POST: /traxes
-  post "/traxes" do
-    redirect "/traxes"
-  end
+    end
 
-  # GET: /traxes/5
-  get "/traxes/:id" do
-    erb :"/traxes/show"
-  end
+    
+    #edit
+    get '/traxes/:id/edit' do
+        if logged_in?
+            @traxes = Traxes.find(params[:id])
+            if @traxes.user_id == session[:user_id]
+             erb :'/traxes/edit'
+            else
+            erb :'traxes'
+            end
+          else
+            erb :'users/sign_in'
+          end
+        end
+        
+    end
 
-  # GET: /traxes/5/edit
-  get "/traxes/:id/edit" do
-    erb :"/traxes/edit"
-  end
+    patch '/trax/:id' do
+        if params.value.any? {|value|value == ""}
+            redirect to "/traxes/#{@traxes.id}/edit" 
+        else   
+        @traxes = Traxes.find(params[:id])
+        @traxes = Traxes.update(
+            name: params[:name], date: params[:date], 
+            score: params[:score], location: params[:location], 
+            number: params[:number], interest: params[:interest]
+        )
+        @traxes.save
+        redirect to "/traxes/#{@traxes.id}"
+    end
+    #delete
+    delete '/traxes/:id/delete' do
+     @traxes = Traxes.find(params [:id])  
+     if session[:user_id]
+        @traxes = Traxes.find(params[:id])
+        if @traxes.user_id == session[:user_id]
+          @traxes.delete
+          redirect to '/traxes'
+        else
+          redirect to '/traxes'
+        end
+      else
+        redirect to '/sign_in'
+      end
+    end
 
-  # PATCH: /traxes/5
-  patch "/traxes/:id" do
-    redirect "/traxes/:id"
-  end
-
-  # DELETE: /traxes/5/delete
-  delete "/traxes/:id/delete" do
-    redirect "/traxes"
-  end
 end
